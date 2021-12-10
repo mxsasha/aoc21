63866d4e0f40c53104585f95c90a28ef7dd73464
Sasha Romijn <github@mxsasha.eu>
2021-12-10 20:36:29 +0100

    Refactored syntax scoring part 1

--- syntax-scoring/src/main.rs
+++ syntax-scoring/src/main.rs
@@ -1,43 +1,71 @@
 use std::io::{self, Read};
 
-fn process_line(line: &str) -> Option<char> {
-    let mut symbol_stack: Vec<char> = vec![];
+enum Char {
+    Opening(CharType),
+    Closing(CharType),
+}
 
-    for ch in line.chars() {
-        if ['<', '{', '(', '['].contains(&ch) {
-            symbol_stack.push(ch);
-        } else {
-            let top = symbol_stack.pop();
-            let valid = match top {
-                Some('<') => ch == '>',
-                Some('{') => ch == '}',
-                Some('(') => ch == ')',
-                Some('[') => ch == ']',
-                _ => false,
-            };
-            if !valid {
-                return Some(ch);
-            }
+#[derive(Debug, PartialEq)]
+enum CharType {
+    AngleBracket,
+    Parenthesis,
+    CurlyBrace,
+    SquareBracket,
+}
+
+impl TryFrom<char> for Char {
+    type Error = ();
+
+    fn try_from(value: char) -> Result<Char, Self::Error> {
+        match value {
+            '<' => Ok(Char::Opening(CharType::AngleBracket)),
+            '>' => Ok(Char::Closing(CharType::AngleBracket)),
+            '(' => Ok(Char::Opening(CharType::Parenthesis)),
+            ')' => Ok(Char::Closing(CharType::Parenthesis)),
+            '{' => Ok(Char::Opening(CharType::CurlyBrace)),
+            '}' => Ok(Char::Closing(CharType::CurlyBrace)),
+            '[' => Ok(Char::Opening(CharType::SquareBracket)),
+            ']' => Ok(Char::Closing(CharType::SquareBracket)),
+            _ => Err(()),
         }
+    }
+}
+
+impl CharType {
+    fn mismatch_score(&self) -> usize {
+        match self {
+            CharType::AngleBracket => 25137,
+            CharType::Parenthesis => 3,
+            CharType::CurlyBrace => 1197,
+            CharType::SquareBracket => 57,
+        }
+    }
+}
+
+fn process_line(line: &str) -> Option<CharType> {
+    let mut symbol_stack: Vec<CharType> = vec![];
+    let chars: Vec<Char> = line.chars().map(|c| Char::try_from(c).unwrap()).collect();
+    for ch in chars {
+        match ch {
+            Char::Opening(char_type) => symbol_stack.push(char_type),
+            Char::Closing(char_type) => {
+                if symbol_stack.pop().as_ref() != Some(&char_type) {
+                    return Some(char_type);
+                }
+            }
+        };
     }
     None
 }
 
 fn calculate(input: &str) -> usize {
-    let mismatches: Vec<Option<char>> = input.trim().lines().map(process_line).collect();
+    let mismatches: Vec<Option<CharType>> = input.trim().lines().map(process_line).collect();
     println!("mismatches: {:?}", mismatches);
-    let scores: Vec<usize> = mismatches
+    mismatches
         .iter()
         .flatten()
-        .map(|ch| match ch {
-            '>' => 25137,
-            '}' => 1197,
-            ')' => 3,
-            ']' => 57,
-            _ => panic!("aaaaaah"),
-        })
-        .collect();
-    scores.iter().sum()
+        .map(|ct| ct.mismatch_score())
+        .sum()
 }
 
 fn main() {
