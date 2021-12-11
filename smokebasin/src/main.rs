ae41408e44d604e6430aeceafdefb99bd98a65df
Sasha Romijn <github@mxsasha.eu>
2021-12-11 22:56:24 +0100

    smokebasin part 2

--- smokebasin/src/main.rs
+++ smokebasin/src/main.rs
@@ -1,4 +1,6 @@
 use std::io::{self, Read};
+use std::collections::HashMap;
+
 
 #[derive(Debug, PartialEq, Clone, Hash, Eq)]
 struct HeightMap {
@@ -8,18 +10,20 @@ struct HeightMap {
 
 impl From<&str> for HeightMap {
     fn from(input: &str) -> Self {
+        let points: Vec<Vec<u32>> = input
+            .trim()
+            .lines()
+            .map(|line| {
+                line.trim()
+                    .chars()
+                    .map(|c| c.to_digit(10).unwrap())
+                    .collect()
+            })
+            .collect();
+        let (len_x, len_y) = (points[0].len(), points.len());
         HeightMap {
-            basin_assignments: vec![],
-            points: input
-                .trim()
-                .lines()
-                .map(|line| {
-                    line.trim()
-                        .chars()
-                        .map(|c| c.to_digit(10).unwrap())
-                        .collect()
-                })
-                .collect(),
+            points,
+            basin_assignments: vec![vec![0; len_x]; len_y],
         }
     }
 }
@@ -28,42 +32,79 @@ impl HeightMap {
     fn size(&self) -> (usize, usize) {
         (self.points[0].len(), self.points.len())
     }
+
     fn is_low(&self, x: usize, y: usize) -> bool {
         let mut is_low = true;
-        let (len_x, len_y) = self.size();
-        if x > 0 && self.points[y][x - 1] < self.points[y][x] {
-            is_low = false;
+        for (neigh_x, neigh_y) in self.get_neighbors(x, y) {
+            if self.points[neigh_y][neigh_x] < self.points[y][x] {
+                is_low = false;
+            }
         }
-        if y > 0 && self.points[y - 1][x] < self.points[y][x] {
-            is_low = false;
-        }
-        if x < len_x - 1 && self.points[y][x + 1] < self.points[y][x] {
-            is_low = false;
-        }
-        if y < len_y - 1 && self.points[y + 1][x] < self.points[y][x] {
-            is_low = false;
-        }
-
         is_low
     }
+
+    fn fill_basin(&mut self, x: usize, y: usize, basin_id: u32) {
+        if self.get(x, y) == 9 || self.basin_assignments[y][x] != 0 {
+            return;
+        }
+        self.basin_assignments[y][x] = basin_id;
+        for (neigh_x, neigh_y) in self.get_neighbors(x, y) {
+            self.fill_basin(neigh_x, neigh_y, basin_id);
+        }
+    }
+
+    fn get_neighbors(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
+        let mut neighbors = vec![];
+        let (len_x, len_y) = self.size();
+        if x > 0 {
+            neighbors.push((x - 1, y));
+        }
+        if y > 0 {
+            neighbors.push((x, y - 1));
+        }
+        if x < len_x - 1 {
+            neighbors.push((x + 1, y));
+        }
+        if y < len_y - 1 {
+            neighbors.push((x, y + 1));
+        }
+        neighbors
+    }
+
     fn get(&self, x: usize, y: usize) -> u32 {
         self.points[y][x]
+    }
+
+    fn basin_sizes(&self) -> HashMap<u32, u32> {
+        let mut map = HashMap::new();
+        let basin_ids: Vec<u32> = self.basin_assignments.iter().flatten().copied().filter(|id| *id != 0).collect();
+        for basin_id in basin_ids {
+            *map.entry(basin_id).or_default() += 1;
+         }
+         map
     }
 }
 
 fn calculate(input: &str) -> u32 {
-    let heightmap = HeightMap::from(input);
+    let mut heightmap = HeightMap::from(input);
     let mut low_point_score = 0;
     let (len_x, len_y) = heightmap.size();
 
+    let mut basin_id = 1;
     for x in 0..len_x {
         for y in 0..len_y {
             if heightmap.is_low(x, y) {
+                heightmap.fill_basin(x, y, basin_id);
+                basin_id += 1;
                 low_point_score += heightmap.get(x, y) + 1;
             }
         }
     }
-    low_point_score
+    let mut basin_sizes: Vec<u32> = heightmap.basin_sizes().values().copied().collect();
+    basin_sizes.sort();
+    basin_sizes.reverse();
+    basin_sizes.iter().take(3).fold(1, |acc, x| acc * x)
+    // low_point_score
 }
 
 fn main() {
@@ -86,6 +127,6 @@ mod tests {
 8767896789
 9899965678",
         );
-        assert_eq!(count, 15);
+        assert_eq!(count, 1134);
     }
 }
