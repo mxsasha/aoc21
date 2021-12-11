ee05431800b504f812d3a0e4cfa54acf73cb6a07
Sasha Romijn <github@mxsasha.eu>
2021-12-11 23:14:54 +0100

    refactor smokebasin to remove magic number

--- smokebasin/src/main.rs
+++ smokebasin/src/main.rs
@@ -1,24 +1,48 @@
-use std::io::{self, Read};
+use std::cmp::Ordering;
+use std::cmp::PartialOrd;
 use std::collections::HashMap;
+use std::io::{self, Read};
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Height {
+    Regular(u32),
+    Peak,
+}
 
-#[derive(Debug, PartialEq, Clone, Hash, Eq)]
+impl From<char> for Height {
+    fn from(input: char) -> Self {
+        let digit = input.to_digit(10).unwrap();
+        if digit == 9 {
+            return Height::Peak;
+        }
+        Height::Regular(digit)
+    }
+}
+
+impl PartialOrd for Height {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        use Height::*;
+        match (self, other) {
+            (&Regular(ref a), &Regular(ref b)) => Some(a.cmp(b)),
+            (&Regular(ref _a), &Peak) => Some(Ordering::Less),
+            (&Peak, &Regular(ref _b)) => Some(Ordering::Greater),
+            (&Peak, &Peak) => Some(Ordering::Equal),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct HeightMap {
-    points: Vec<Vec<u32>>,
+    points: Vec<Vec<Height>>,
     basin_assignments: Vec<Vec<u32>>,
 }
 
 impl From<&str> for HeightMap {
     fn from(input: &str) -> Self {
-        let points: Vec<Vec<u32>> = input
+        let points: Vec<Vec<Height>> = input
             .trim()
             .lines()
-            .map(|line| {
-                line.trim()
-                    .chars()
-                    .map(|c| c.to_digit(10).unwrap())
-                    .collect()
-            })
+            .map(|line| line.trim().chars().map(Height::from).collect())
             .collect();
         let (len_x, len_y) = (points[0].len(), points.len());
         HeightMap {
@@ -44,7 +68,7 @@ impl HeightMap {
     }
 
     fn fill_basin(&mut self, x: usize, y: usize, basin_id: u32) {
-        if self.get(x, y) == 9 || self.basin_assignments[y][x] != 0 {
+        if self.get(x, y) == Height::Peak || self.basin_assignments[y][x] != 0 {
             return;
         }
         self.basin_assignments[y][x] = basin_id;
@@ -71,23 +95,29 @@ impl HeightMap {
         neighbors
     }
 
-    fn get(&self, x: usize, y: usize) -> u32 {
+    fn get(&self, x: usize, y: usize) -> Height {
         self.points[y][x]
     }
 
     fn basin_sizes(&self) -> HashMap<u32, u32> {
         let mut map = HashMap::new();
-        let basin_ids: Vec<u32> = self.basin_assignments.iter().flatten().copied().filter(|id| *id != 0).collect();
+        let basin_ids: Vec<u32> = self
+            .basin_assignments
+            .iter()
+            .flatten()
+            .copied()
+            .filter(|id| *id != 0)
+            .collect();
         for basin_id in basin_ids {
             *map.entry(basin_id).or_default() += 1;
-         }
-         map
+        }
+        map
     }
 }
 
 fn calculate(input: &str) -> u32 {
     let mut heightmap = HeightMap::from(input);
-    let mut low_point_score = 0;
+    // let mut low_point_score = 0;
     let (len_x, len_y) = heightmap.size();
 
     let mut basin_id = 1;
@@ -96,14 +126,14 @@ fn calculate(input: &str) -> u32 {
             if heightmap.is_low(x, y) {
                 heightmap.fill_basin(x, y, basin_id);
                 basin_id += 1;
-                low_point_score += heightmap.get(x, y) + 1;
+                // low_point_score += heightmap.get(x, y) + 1;
             }
         }
     }
     let mut basin_sizes: Vec<u32> = heightmap.basin_sizes().values().copied().collect();
-    basin_sizes.sort();
+    basin_sizes.sort_unstable();
     basin_sizes.reverse();
-    basin_sizes.iter().take(3).fold(1, |acc, x| acc * x)
+    basin_sizes.iter().take(3).product()
     // low_point_score
 }
 
